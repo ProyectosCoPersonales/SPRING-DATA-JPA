5e191b96fe268c32760c55d6ee4abbb16232ea32
JordanDeev <prograllumitaxi@outlook.com>
2024-09-26 18:56:31 -0500

    first commit

---
+++ postresql/src/test/java/com/postresql/PostresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
